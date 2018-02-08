6bb226b6db5c9b95b52b15f6915e7f5eeed8f7a1
Justin Tooley <jstooley823@gmail.com>
2018-02-08 22:17:55 +0000

    automatically backed up by learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,10 +1,13 @@
 class LineItemsController < ApplicationController
   def create
     if logged_in?
+
       if !current_user.current_cart
         current_user.create_current_cart
-      end 
+      end
+
       line_item = current_user.current_cart.add_item(params[:item_id])
+      
       if line_item.save
         redirect_to cart_path(current_user.current_cart), {notice: 'Item added to cart!'}
       else
