b89561d8a95ca4ef544822c9f38db751a6372bd7
Justin Tooley <jstooley823@gmail.com>
2018-02-08 19:59:48 +0000

    automatically backed up by learn

--- db/migrate/20140805184430_create_line_items.rb
+++ db/migrate/20140805184430_create_line_items.rb
@@ -1,7 +1,7 @@
 class CreateLineItems < ActiveRecord::Migration
   def change
     create_table :line_items do |t|
-      t.integr :quantity. default: 1
+      t.integer :quantity. default: 1
       t.integer :cart_id
       t.integer :item_id
     end
