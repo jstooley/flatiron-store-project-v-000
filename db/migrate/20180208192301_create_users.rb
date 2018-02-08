89ea9c5ea755fad02852eadbc1aa7dfc1352937f
Justin Tooley <jstooley823@gmail.com>
2018-02-08 20:35:19 +0000

    automatically backed up by learn

--- db/migrate/20180208192301_create_users.rb
+++ db/migrate/20180208192301_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
+
       
-      t.string :password_digest
       t.integer :current_cart_id
     end
   end
