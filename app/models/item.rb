0a4ea4222739bfad4ea9acab398f51855c11d7f1
Justin Tooley <jstooley823@gmail.com>
2018-02-08 21:18:31 +0000

    automatically backed up by learn

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,12 @@
 class Item < ActiveRecord::Base
   has_many :line_items
   belongs_to :category
+
+  def self.available_items
+    where('inventory > ?', 0)
+  end
+
+  def remove(amount)
+    update(inventory: inventory - amount)
+  end
 end
