dce5adb0959e6246052dc06dfdd43deae08a5e1a
Justin Tooley <jstooley823@gmail.com>
2018-02-08 21:47:15 +0000

    automatically backed up by learn

--- app/helpers/carts_helper.rb
+++ app/helpers/carts_helper.rb
@@ -1,10 +1,7 @@
 module CartsHelper
   def current_cart
-    if cart = Cart.find_by(id: current_user.current_cart_id)
-      cart
-    else
-      cart = create_current_cart
-    end
+    Cart.find_by(id: current_user.current_cart_id)
+
   end
 
   def create_current_cart
