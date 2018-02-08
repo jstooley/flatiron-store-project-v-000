ff6b617159372abe8633c7ed0e1cbad0fc58eb94
Justin Tooley <jstooley823@gmail.com>
2018-02-08 21:59:59 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,7 +12,11 @@ class User < ActiveRecord::Base
     binding.pry
     User.find_by(id: session[:user_id])
   end
-
+  def create_current_cart
+    new_cart = cart.create
+    current_user.current_cart_id = new_cart.id
+    new_cart.save
+  end
   def remove_cart
     self.current_cart_id = nil
     save
