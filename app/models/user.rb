2185c8fae1fff8005ef9cda92b57b9124e046cb2
Justin Tooley <jstooley823@gmail.com>
2018-02-08 22:01:11 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,7 +15,7 @@ class User < ActiveRecord::Base
 
   def create_current_cart
     new_cart = carts.create
-    current_user.current_cart_id = new_cart.id
+    self.current_cart_id = new_cart.id
     save
   end
 
