c918c66f2f33451a1abb28730e32bf877f113659
Justin Tooley <jstooley823@gmail.com>
2018-02-08 22:18:04 +0000

    automatically backed up by learn

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -7,12 +7,13 @@ class LineItemsController < ApplicationController
       end
 
       line_item = current_user.current_cart.add_item(params[:item_id])
-      
+
       if line_item.save
         redirect_to cart_path(current_user.current_cart), {notice: 'Item added to cart!'}
       else
         redirect_to store_path, {notice: 'Unable to add item'}
       end
+      
     else
       redirect_to new_user_session_path
     end
