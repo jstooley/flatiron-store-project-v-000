da37a4a51a0facf3e5a74868484cfeac1a769d7c
Justin Tooley <jstooley823@gmail.com>
2018-02-08 20:45:55 +0000

    automatically backed up by learn

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,7 +3,7 @@ class CategoriesController < ApplicationController
   def index
   end
   def show
-    @category = Category.find_by(id: params(:id))
+    @category = Category.find(id: params(:id))
   end
 
 end
